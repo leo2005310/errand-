66d64c1541dff73491cab26720a1466b3afd20f2
leo2005310 <107912599+leo2005310@users.noreply.github.com>
2024-06-23 10:17:00 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+import App from './App'
+import '@/static/css/global.css'
+import apiConfig from '@/config.js'
+ 
+Vue.prototype.$baseUrl = process.env.NODE_ENV === "development" ? apiConfig.dev.baseUrl : apiConfig.prod.baseUrl
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+import request from '@/utils/request.js'
+Vue.prototype.$request = request
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
